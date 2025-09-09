6b054473b34b085e9bda4bdc6891ba8ceacfb253
Beatrizpenha1 <abeatrizpenha1@gmail.com>
2025-09-09 16:14:08 -0300

    adicionando cadastro e listagem de contatos

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Contato(models.Model):
+    nome = models.CharField(max_length=20)
+    sobrenome = models.CharField(max_length=20)
+    descricao = models.TextField(max_length=255)
+    telefone = models.IntegerField()
+    imagem = models.ImageField(upload_to='core_imagens/%Y/%m/', blank=True, null=True)
