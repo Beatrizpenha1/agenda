a10d70f7ff6eb08d2880aafa34811c1b9a3afd8c
Beatrizpenha1 <abeatrizpenha1@gmail.com>
2025-10-21 14:01:06 -0300

    adicionando excluir contato

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     #path('', views.index, name='index'),
     path('add_contato/',views.add_contato,
          name='add_contato'),
-    path('listar_contato/',views.listar_contato,name='listar_contato')
+    path('listar_contato/',views.listar_contato,name='listar_contato'),
+    path('editarContato/<int:id>',views.editarContato,name='editarContato'),
+    path('excluirContato/<int:id>',views.excluirContato,name='excluirContato'),
+
+
 ]
